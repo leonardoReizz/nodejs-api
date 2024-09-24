f0ed9187e4a9e93808907c139191c908d2316d4b
Leonardo Reis <amarantee.reis@gmail.com>
2024-09-23 22:17:57 -0300

    feat: setup express server

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { routes } from "./routes";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { env } from "./env";
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.express.use(
+      cors({
+        origin:
+          env.NODE_ENV === "dev"
+            ? [
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "http://localhost:3002",
+              ]
+            : [],
+      })
+    );
+    this.express.use(express.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use(bodyParser.json());
+  }
+
+  private routes(): void {
+    this.express.use(routes);
+  }
+}
+
+export default new App().express;
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import app from "./app";
+import dotenv from "dotenv";
+import { ZodError } from "zod";
+import { env } from "./env";
+import { BadRequestException } from "./errors/BadRequestException";
+import { UnauthorizedException } from "./errors/UnauthorizedException";
+
+dotenv.config();
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      message: "Validation error.",
+      issues: error.format(),
+    });
+  }
+
+  if (error instanceof UnauthorizedException) {
+    return res.status(401).json({
+      message: error.message,
+    });
+  }
+
+  if (error instanceof BadRequestException) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({ message: error.message });
+});
+
+if(env.NODE_ENV !== "test") {
+  app.listen(env.PORT, () => {
+    console.log("Express connected, port: " + env.PORT);
+  });
+}
+
+export { app };
